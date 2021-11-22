8d193ca357d96a70a216a3fda85dd8375c6e22b8
David Reusch <dbreusch@me.com>
2021-11-21 17:02:03 -0800

    Added user schema so courses can track creator

---
+++ models/user.js
@@ -0,0 +1,24 @@
+// users-api: database schema
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+// const passport = require("passport");
+// const passportLocalMongoose = require("passport-local-mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
+  type: { type: String, required: false },
+  active: { type: Boolean, required: false },
+  dateAdded: { type: Date, required: false },
+  lastLogin: { type: Date, required: false },
+  courses: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Course' }]
+});
+
+// userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
